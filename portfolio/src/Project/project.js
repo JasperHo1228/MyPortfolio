2d9e50ee237758342ec206c5d6d7f5081cfc8f3f
Jasper Ho <49902173+JasperHo1228@users.noreply.github.com>
2023-07-27 04:57:00 +0100

    Done with the Version one of Project page

--- portfolio/src/Project/project.js
+++ portfolio/src/Project/project.js
@@ -5,22 +5,28 @@ import { project_json } from './project_json';
 function project(){
     const project = ([...project_json]); 
 return(
-
         <div id="Project">
             <h1 className="project_title">Project</h1>
             <div className="project-container">
                 {project.map((link) => (
-                     <article className='project_item'>  
+                    <div className='frame'>
+                     <div className='project_item'>  
                          <div className='project_img'>
                             <img src={link.image} alt=""/>
                         </div>
-                            <h3>{link.title}</h3>
-                            <div className='project_link'>
+
+                        {/* I dont not why I must use project_link to show the title */}
+                        <div className='project_link'>
+                            <div className='title'>
+                                <h3>{link.title}</h3>
+                            </div>
+                            </div>
+                         <div className='project_link'> 
                                 <a href={link.youtube_link} className='btn' target="_blank" rel="noreferrer">Youtube Demo</a>
                                 <a href={link.source_code} className='btn2' target="_blank" rel="noreferrer">Source Code</a>
                             </div>
-                        
-                      </article>
+                      </div>
+                    </div>
                 ))}
             </div>
         </div>
