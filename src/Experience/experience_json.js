4a241d9286f2ed4b5b059da198b7952994617099
Jasper Ho <49902173+JasperHo1228@users.noreply.github.com>
2026-09-01 21:05:35 +0100

    add_workExp
    - Add work experience in experience_json file

--- src/Experience/experience_json.js
+++ src/Experience/experience_json.js
@@ -3,7 +3,44 @@ export const experience_json = [
     {
         id:1,
         icon:"workIcon",
-        title:"iOS App Developer",
+        title:"Programmer II", 
+        subtitle:"Shoalter Automation Limited",
+        date:"(Octber 2025 – current)",
+        position:"left_position",
+        arrow:"left-arrow",
+        contribution:(
+          <div>      
+              • Worked with **Project Managers** to analyse requirements and deliver technical solutions that meet user needs.
+                 <br/>
+                 <br/>         
+              • Investigated and resolved **production issues**, including API errors, data inconsistencies, and performance problems.
+          </div>
+          ),
+    },  
+    {
+        id:2,
+        icon:"workIcon",
+        title:"Junior Developer", 
+        subtitle:"Shoalter Automation Limited",
+        date:"(January 2024 – October 2025)",
+        position:"right_position",
+        arrow:"right-arrow",
+        contribution:(
+          <div> 
+              • Developed Java/Spring Boot features for WMS, including warehouse workflows and business logic.
+                 <br/>
+                 <br/>
+              •  Optimised **MySQL/Hibernate queries**, reducing operation time and eliminating **N+1 query issues**.
+                 <br/>
+                 <br/>             
+              • Integrated RabbitMQ and REST APIs for communication between WMS and external services.
+          </div>
+          ),
+    },
+    {
+        id:3,
+        icon:"workIcon",
+        title:"iOS App Developer(Intership)",
         subtitle:"FoodCLUB",
         date:"(March 2023 – July 2023)",
         position:"left_position",
@@ -19,7 +56,7 @@ export const experience_json = [
           ),
     },
     {
-        id:2,
+        id:4,
         icon:"schoolIcon",
         title:"MSc Advanced Computer Science",
         subtitle:"University of Leeds",
@@ -29,7 +66,7 @@ export const experience_json = [
         contribution:"• Graduated with Distinction, earning a Master of Science degree.",
     },
     {
-        id: 3,
+        id: 5,
         icon: "schoolIcon",
         title: "BSc Computer Science, and Information Engineering",
         subtitle:"National Chin-Yi University of Technology - Taiwan",
